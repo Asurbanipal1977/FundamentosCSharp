7292cf72165a219878c169111d20fee465103cfa
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-12-01 20:46:17 +0100

    Closure

---
+++ Closure/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Closure
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Action fn = EjemploClosure(2);
+			HacerAlgo(fn);
+			HacerAlgo(fn);
+			HacerAlgo(fn);
+		}
+
+		static Action EjemploClosure(int maximo)
+		{
+			int i = 0;
+			Console.WriteLine("Se inicializa EjemploClosure");
+
+			return () =>
+			{
+				if (i < 2)
+				{
+					Console.WriteLine("Se ejecuta Ejemplo de Closure");
+				}
+				else
+				{
+					Console.WriteLine("Ya no se ejecuta Ejemplo de Closure");
+				}
+				i++;
+			};
+		}
+
+		static void HacerAlgo(Action fn)
+		{
+			Console.WriteLine("Hace Algo Inicio");
+			fn();
+			Console.WriteLine("Hace Algo Fin");
+		}
+	}
+}
