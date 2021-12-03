959e1fe705e1d7a02a97dffe86abca09cf4dcab0
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-12-03 16:31:17 +0100

    Extended properties

---
+++ ExtendedProperties/Clases.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedProperties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+
+    public static class Discount
+    {
+        public static double Get(Person person, double amount) =>
+            person switch
+            {
+                { Address.City: "Madrid" } => amount - amount * 0.04,
+                { Address.City: "Barcelona" } => amount - amount * 0.02,
+                { Address.City: "Toledo" } => amount - amount * 0.01,
+                _ => amount - amount * 0.005
+            };
+    }
+}
---
+++ ExtendedProperties/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExtendedProperties;
+
+Person person = new Person()
+{
+    Name = "Juan",
+    Address = new Address() { City = "Madrid", State = "España" }
+};
+Console.WriteLine(Discount.Get(person, 20));
