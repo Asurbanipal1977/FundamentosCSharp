3216177dfeb321e0d4ba5d5e360122d71fb763b5
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-12-10 10:02:24 +0100

    yield

--- Closure/Program.cs
+++ Closure/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Closure
 {
@@ -10,6 +11,19 @@ namespace Closure
 			HacerAlgo(fn);
 			HacerAlgo(fn);
 			HacerAlgo(fn);
+
+			//Ejemplo de yield
+			var i = 0;
+			foreach (int arg in bucleSinYield())
+            {
+				Console.WriteLine($"Console externo {++i}");
+            }
+
+			i = 0;
+			foreach (int arg in bucleConYield())
+			{
+				Console.WriteLine($"Console externo {++i}");
+			}
 		}
 
 		static Action EjemploClosure(int maximo)
@@ -36,6 +50,28 @@ namespace Closure
 			Console.WriteLine("Hace Algo Inicio");
 			fn();
 			Console.WriteLine("Hace Algo Fin");
+		}
+
+
+		public static IEnumerable<int> bucleSinYield()
+        {
+			List<int> auxiliar = new List<int>();
+			for (int i=0;i<10;i++)
+            {
+				Console.WriteLine($"Console interno sin Yield {i+1}");
+				auxiliar.Add(i);
+            }
+
+			return auxiliar;
+        }
+
+		public static IEnumerable<int> bucleConYield()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine($"Console interno con Yield {i + 1}");
+				yield return i;
+			}
 		}
 	}
 }
