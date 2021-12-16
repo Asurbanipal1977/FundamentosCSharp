b9443d51a074746a2ede32f54d410cfcbeb5f4b5
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-12-17 00:41:30 +0100

    Hilos usando recursos compartidos

---
+++ HilosRecursosCompartidos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HilosRecursosCompartidos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Todos van al mismo fichero
+            string path = "result.txt";
+            var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+            Parallel.For(1, 101, new ParallelOptions { MaxDegreeOfParallelism = 10 },
+                (i) => Write(i.ToString(), fs));
+        }
+
+        static void Write (string content, FileStream fs)
+        {
+            lock (fs)
+            {
+                byte[] bContent = new UTF8Encoding(true).GetBytes(content + Environment.NewLine);
+                fs.Write(bContent, 0, bContent.Length);
+                fs.Flush();
+            }
+        }
+    }
+}
