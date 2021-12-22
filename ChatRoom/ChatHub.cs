1208bcdfb0644f68da17b20e47f2d4ed90548ad7
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-12-22 17:57:38 +0100

    Chat

---
+++ ChatRoom/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatRoom
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+
+        }
+
+        public async Task AddToGroup (string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId,room);
+            await Clients.Group(room).SendAsync("ShowUser", $"Se conectó {Context.ConnectionId}");
+        }
+    }
+}
---
+++ ChatRoom/Controllers/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> rooms = new Dictionary<int, string>()
+        {
+            {1,"Deporte"},
+            {2,"Música"},
+            {3,"Actualidad"}
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
