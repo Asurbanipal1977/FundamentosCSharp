c9f19cc3c0079671858d3a575484f10df1a8cbde
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-12-22 02:02:31 +0100

    blazor y signalr

---
+++ BlazorSignalR/Server/TextHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace BlazorSignalR.Server
+{
+    public class TextHub : Hub
+    {
+        public async Task Send(string text)
+        {
+            await Clients.All.SendAsync("RecibirInformacion",text);
+        }
+    }
+}
