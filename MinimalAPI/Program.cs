206ca777bf6cb74e2ce57aefdc69bfac5dc73eb5
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2022-01-16 14:18:07 +0100

    Pruebas Unitarias

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -55,4 +55,8 @@ app.Run();
 
 
 // Make the implicit Program class public so test projects can access it
-public partial class Program { }
+
+namespace MinimalAPI
+{
+    public partial class Program { }
+}
